615508303d7d8bdc9580937e77e77416305803e2
Mark Archual <mja61@zips.uakron.edu>
2017-01-06 12:11:56 -0500

    Beefs up grunt file with more automation tasks to make development easier

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function(grunt) {
 
     // requirejs compile options
     var compileOptions = {
-
         mainConfigFile: 'public/scripts/main.js',
         baseUrl: 'public/scripts',
         include: ['main'],
@@ -16,12 +15,40 @@ module.exports = function(grunt) {
             if(/(.*)js\/modules\/(.*)/.test(path)) return contents.replace(/console.log(.*);/g, ';');
             return contents;
         }
-    }
+    };
 
-    //Initializing the configuration object
+    //Initializing the configuration object, required for grunt to run
     grunt.initConfig({
 
-        // Task configuration
+    // Task configuration
+
+        //Testing
+        jshint: {
+            all: ['Gruntfile.js', 'server.js', 'database.js', 'routes/*.js', 'public/scripts/**/*.js'],
+            server: ['server.js', 'database.js', 'routes/*.js'],
+            public: ['public/scripts/**/*.js']
+        },
+
+        //Code Compilation
+        //clean the distribution folder so nothing old hangs around
+        clean: {
+            dist: {
+                src: ['dist']
+            },
+            css: {
+                src: ['dist/*.css', 'public/stylesheets/*.css']
+            },
+            js: {
+                src: ['dist/*.js']
+            }
+        },
+
+        requirejs: {
+            compile: {
+                options : compileOptions //run with the compile options configured above
+            }
+        },
+
         less: {
             development: {
                 options: {
@@ -34,7 +61,7 @@ module.exports = function(grunt) {
             },
             production: {
                 options: {
-                    cleancss: true, // minify css
+                    cleancss: true, // clean css
                     compress: true, // minify css
                 },
                 files: {
@@ -43,16 +70,36 @@ module.exports = function(grunt) {
                 }
             }
         },
-        requirejs: {
-            compile: {
-                options : compileOptions
+
+        //Server
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'server.js',
+                    nodeArgs: ['--debug'],
+                    watchedFolders: ['routes', 'server.js', 'database.js'],
+                    env: {
+                        PORT: '3300'
+                    }
+                }
             }
         },
+
+        //Watchers to be executed upon file changes
+        concurrent: {
+          dev: {
+              tasks: ['nodemon:dev', 'watch'],
+              options: {
+                  logConcurrentOutput: true
+              }
+          }
+        },
+
         watch: {
             less: {
                 // Watch all .less files from the styles directory)
                 files: ['public/styles/*.less'],
-                tasks: ['less'],
+                tasks: ['clean:css', 'less'],
                 // Reloads the browser
                 options: {
                     livereload: true
@@ -60,8 +107,8 @@ module.exports = function(grunt) {
             },
             requirejs: {
                 // Watch only main.js so that we do not constantly recompile the .js files
-                files: [ 'public/scripts/main.js' ],
-                tasks: [ 'requirejs' ],
+                files: ['public/scripts/main.js'],
+                tasks: ['clean:js', 'requirejs'],
                 // Reloads the browser
                 options: {
                     livereload: true
@@ -70,12 +117,39 @@ module.exports = function(grunt) {
         }
     });
 
-    // Plugin loading
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    //Load all of our dev dependencies for grunt to run
+    require('load-grunt-tasks')(grunt);
 
+    // Task definitions, you can type the first argument into the command line to execute the corresponding task
+    // eg.) grunt server runs nodemon:dev, default runs everything
 
-    // Task definition
-    grunt.registerTask('default', ['less', 'requirejs']);
+    //task registration follows the same pattern as their definitions\
+
+    //default - run everything
+    grunt.registerTask('default', []);
+
+    //compilation
+    grunt.registerTask('comp', ['requirejs', 'less'])
+    grunt.registerTask('comp-clean', ['clean:css']);
+    grunt.registerTask('comp-requirejs', ['requirejs']);
+    grunt.registerTask('comp-less', ['less']);
+    grunt.registerTask('comp-less-dev', ['less:development']);
+    grunt.registerTask('comp-less-prod', ['less:production']);
+
+    //testing
+    grunt.registerTask('test', ['jshint:all']) //this line will include all testing frameworks
+    grunt.registerTask('test-jshint', ['jshint:all']);
+    grunt.registerTask('test-public-jshint', ['jshint:public']);
+    grunt.registerTask('test-server-jshint', ['jshint:server']);
+
+    //server
+    grunt.registerTask('server', ['nodemon:dev']);
+
+    //watchers
+    grunt.registerTask('watch',['watch'])
+    grunt.registerTask('',[''])
+
+    //helpers - use this to run several tasks at once during development
+    grunt.registerTask('devmode',['concurrent:dev'])
+
 };
